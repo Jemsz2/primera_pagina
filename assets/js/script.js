be89ed742250321e30f1d4363bcb1770f8353e8f
Jemsz2 <jmesa47382@gmail.com>
2023-12-07 12:40:47 -0400

    envio al repostiorio

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+let myVariable = 7
+
+let tipoNumero = 45
+let tipoCadena = "45"
+let tipoBooleano = false
+
+let tipoIndefinido = undefined
+let tipoVacío = null
+
+let tipoLista = [
+50,
+77,
+29,
+100,
+47,
+"jose",
+"hp",
+"philips",
+
+
+]
+
+let tipoObjeto = {
+name: "jose",
+email: "jmes470@gmail.com",
+pass: "1234",
+
+
+}
+
+
+let usuario = [
+"jose",
+"jmes479@gmail.com",
+"1234",
+
+]
+
+
+console.log(tipoLista)
