f74ae2b8d0b1fb760f3bc39fe087ba2fa9e9a0ee
Jemsz2 <jmesa47382@gmail.com>
2023-12-08 16:56:55 -0400

    mensajeriaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,4 +37,99 @@ let usuario = [
 ]
 
 
-console.log(tipoLista)
+/** 
+ *  OPERADORES MATEMATICOS
+ * 
+ * + SUMA O ADICION
+ * - MENOS O SUSTRACCION
+ * * MULTIPLICACIÓN
+ * / DIVISION
+ * % SE UTILIZA PARA SACAR EL RESIDUO DE LA DIVISON (EL RESTO DE LA DIVISION ENTRE)
+ * 
+ *  OPERADORES LOGICOS
+ * 
+ * = (NO ES UN IGUAL) OPERADOR DE ASIGNACIÓN
+ * == ESTO SI ES IGUALDAD
+ * === IGUALDAD CON TIPADO ES PARA COMPARAR LOS TIPOS DE DATOS
+ * != NEGACION (NO ES IGUAL)
+ * !== NEGACION CON TIPADO
+ * > MAYOR QUE
+ * < MENOR QUE 
+ * >= MAYOR O IGUAL QUE 
+ * <= MENOR O GIUAL QUE
+ * 
+*/
+
+let variable1 = 5
+let variable2 = undefined
+let variable3 = 8
+let variable4 = 2
+let variable5 = false
+let variable6 = 0
+
+
+
+console.log()
+
+
+if(true) console.log("Se cumplió")
+else console.log("No se cumple")
+
+/**
+ * Operador ternario 
+ */
+
+/* 
+true ? 
+    console.log("Se cumple")
+    :console.log("No se cumple")
+*/
+
+/**
+ * cree una funcion que valide si un numero es par o impar. De ser par, retorne "Par": string, de ser impar, retorne "Impar": string
+ * 
+ */
+
+/*
+function MyFuncion (num1, num2, validacion) {
+
+return validacion 
+     ? num1 + num2 
+     : num1 - num2
+
+}
+
+console.log( MyFuncion(3, 8, true) )
+*/
+
+
+
+
+
+
+
+
+/*
+let Callback = () => {
+  console.log('Se ejecutó el callback')
+
+}
+
+
+
+
+
+setTimeout( Callback , 5000 )
+
+*/
+
+
+
+
+console.log(axios.get("https://leonardoapi.onrender.com/music"))
+
+axios.get("https://leonardoapi.onrender.com/music") 
+    .then((res) => {
+    console.log(res.data)
+
+})
